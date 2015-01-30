2cfe45806dc7fb0add379ba38df2d274cd74e83b
Daniel Sellers <daniel@designfrontier.net>
2015-01-30 11:41:05 -0700

    fixing stuff

--- index.js
+++ index.js
@@ -2,25 +2,10 @@ var express = require('express')
 	, app = express()
 	, server
 
-	, getTranslation = function (string, language) {
-		var translationDictionary = {
-			'Hello': {
-				es: 'Hola'
-				, du: 'Guten tag'
-				, ja: '今日は'
-			}
-			, 'Welcome to class': {
-				es: 'Bienvenidos a la clase'
-				, du: 'Willkommen in der Klasse'
-				, ja: 'クラスへの歓迎'
-			}
-		}
-
-		return translationDictionary[string][language] || string;
-	};
+	, lang = require('wnc-lang-daniel');
 
 app.get('/', function (req, res) {
-	res.send(getTranslation('Hello', req.query.lang) + '. ' + getTranslation('Welcome to class', req.query.lang) + '.');
+	res.send(lang.translate('Hello', req.query.lang) + '. ' + lang.translate('Welcome to class', req.query.lang) + '.');
 });
 
 server = app.listen(1337, function () {
